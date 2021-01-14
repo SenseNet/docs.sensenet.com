fe748545dad46665260e603c4adc6af54674591a
herflis <herflis33@gmail.com>
2021-01-14 17:18:12 +0100

    Fix maindpage img's alt attribute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import footer from '../../footerLinks'
 import Logo from '../images/sensenet-footerlogo.png'
 import './styles.css';
+import config from '../../config.js';
 
 const Footer = () => {
   return(
   <div className="Footer">
     <div className="FooterLogo">
     <a href='/'>
-        <img className="img-responsive displayInline" src={Logo} alt={'logo'} />
+        <img className="img-responsive displayInline" src={Logo} alt={config.siteMetadata.title} />
     </a>
     </div>
     <div className="FooterMenu">
